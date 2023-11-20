51b06c469586581ddc72f5f54dc9568d759c0fa4
CousinZe <alex@ckx-ily.cn>
2023-11-20 01:56:00 +0000

    very

--- sample/drill.jl
+++ sample/drill.jl
@@ -1,3 +1,7 @@
 function generic_array_set!(arr::Vector{T}, index::I, value::T) where I <: Integer
     arr[i] = value;
 end
+
+function generic_configure!(env::Dict{String, T} where T, key, value)
+    magic!(env, key, value)
+end
